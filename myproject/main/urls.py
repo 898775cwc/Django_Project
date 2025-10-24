220d1bef083cdefb38b45d8c793800817425c3a8
898775 <898775@my.cwc.ac.uk>
2025-10-24 09:58:08 +0100

    lala

--- myproject/main/urls.py
+++ myproject/main/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-path("", views.startpage, name="Start"),
+path("", views.home, name="Start"),
 path("<int:id>", views.index, name="index")
 #path("v1/", views.v1, name="View 1"),
 ]
--- myproject/main/views.py
+++ myproject/main/views.py
@@ -1,9 +1,13 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
-def startpage(response):
-    return HttpResponse("<h1>Hallo!</h1>")
 def index(response, id):
-    return HttpResponse("<h1>%s</h1>" % id)
+    ls = ToDoList.objects.get(id=id)
+    item = ls.item_set.get(id=4)
+    return render(response, "main/index.html", {})
 
 #def v1(response):
     #return HttpResponse("<h1>View 1!<!h1>")
+
+def home(response):
+    return render(response, "main/home.html", {})
